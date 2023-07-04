1c10a9650a72fe4176a3f06a2a664bb63d854c2d
nahtanpng <nathan.ferreiira13@gmail.com>
2023-07-03 21:22:15 -0300

    Operadores

---
+++ Dart/fundamentos/operadores_1.dart
@@ -0,0 +1,25 @@
+ main() {
+  
+  //Operadorees Aritméticos(binários[precisam de dois operandos]/infix)
+  int a = 7;
+  int b = 3;
+  int resultado = a + b;
+
+  print(resultado);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+  print(33 % 2);
+  print(a + (b * a) - (b / a));
+
+  //Operadores Lógicos
+  bool produtoFragil = true;
+  bool produtoCaro = false;
+
+  print(produtoCaro && produtoFragil); //&& -> AND = E
+  print(produtoCaro || produtoFragil); //|| -> OR = OU
+  print(produtoCaro ^ produtoFragil); //^ -> XOR = OU Exclusivo(Obrigatoriamente um verdadeiro)
+  print(!produtoFragil); //NOT -> Unário/Prefix
+  print(!!produtoCaro);
+}
---
+++ Dart/fundamentos/operadores_3.dart
@@ -0,0 +1,14 @@
+main(){
+
+  int a = 3;
+  int b = 4;
+
+  //Operadores Unários
+  a++; //Posfix
+  --a; //Prefix
+
+  print(a);
+
+  print(a++ == --b);
+  print(a == b);
+}
