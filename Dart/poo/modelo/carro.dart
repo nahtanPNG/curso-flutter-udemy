f4592f6c65d0f8cfe640fb8eddd05606595f15e9
nahtanpng <nathan.ferreiira13@gmail.com>
2023-07-07 18:29:19 -0300

    Desafio Classe carro Um desafio onde utilizei métodos, classes e imports para realizar um teste de velocidade de um carro!

--- Dart/poo/modelo/carro.dart
+++ Dart/poo/modelo/carro.dart
@@ -1,3 +1,30 @@
-class carro {
-  
+class Carro {
+
+  String? nomeCarro;
+  int velocidadeMaxima = 0;
+  int velocidadeAtual = 0;
+
+  Carro([this.nomeCarro, this.velocidadeMaxima = 280]);
+
+  int acelerar() {
+    if(velocidadeAtual + 5 >= velocidadeMaxima){
+      velocidadeAtual = velocidadeMaxima;
+    }else{
+      velocidadeAtual += 5;
+    }
+    return velocidadeAtual;
+  }
+
+  int freiar() {
+    if(velocidadeAtual -5 <= 0){
+      velocidadeAtual = 0;
+    } else{
+      velocidadeAtual -= 5;
+    }
+    return velocidadeAtual;
+  }
+
+  bool estaNoLimite(){
+    return velocidadeAtual == velocidadeMaxima; 
+  }
 }
--- Dart/poo/teste/teste_carro.dart
+++ Dart/poo/teste/teste_carro.dart
@@ -1,6 +1,16 @@
 import '../modelo/carro.dart';
 
 main() {
-  String nomeCarro;
-  final int velocidadeMaxima;
+  var fusca = new Carro("Fuscão Preto", 380);
+
+  while(!fusca.estaNoLimite()){
+    fusca.acelerar();
+    print("O ${fusca.nomeCarro} está acelerando e sua velocidade atual é ${fusca.velocidadeAtual} KM/H");
+  }
+
+  while(fusca.velocidadeAtual > 0){
+    fusca.freiar();
+    print("O ${fusca.nomeCarro} está freiando e sua velocidade atual é ${fusca.velocidadeAtual} KM/H");
+  }
+  print("${fusca.nomeCarro} parou!");
 }
